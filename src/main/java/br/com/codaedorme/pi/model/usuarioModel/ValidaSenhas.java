fae6ab97c71026a5f0a17d44e051230bc3f79881
Kaike Carmona Rodrigues <kkaike371@gmail.com>
2025-02-17 23:22:13 -0300

    sprint1/wip: adicionada a validacao de senha criptografada x String_recebida

--- src/main/java/br/com/codaedorme/pi/model/usuarioModel/ValidaSenhas.java
+++ src/main/java/br/com/codaedorme/pi/model/usuarioModel/ValidaSenhas.java
@@ -1,6 +1,18 @@
 package br.com.codaedorme.pi.model.usuarioModel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.codaedorme.pi.model.Usuario;
+import br.com.codaedorme.pi.repository.UsuarioRepository;
+
+@Service
 public class ValidaSenhas {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public boolean validaSenhas(String senha1, String senha2) {
 
         if (senha1.equals(senha2)) {
@@ -8,5 +20,17 @@ public class ValidaSenhas {
         } else {
             return false;
         }
+    }
+
+    public boolean validaHash(String senhaRecebida, Long usuarioID) {
+        Usuario usuario = usuarioRepository.findById(usuarioID).orElse(null);
+
+        if (usuario == null) {
+            return false;
+        }
+
+        String senhaCriptografada = usuario.getSenha();
+
+        return BCrypt.verifyer().verify(senhaRecebida.toCharArray(), senhaCriptografada.getBytes()).verified;
     }
 }
