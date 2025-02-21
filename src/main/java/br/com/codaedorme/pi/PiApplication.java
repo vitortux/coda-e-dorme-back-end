adb7d36c697ea28e84ed76d04c1dbd13667658d4
Oculos_Azul <eric.romano.santos@gmail.com>
2025-02-21 16:35:49 -0300

    feat: organizacao basica

--- src/main/java/br/com/codaedorme/pi/PiApplication.java
+++ src/main/java/br/com/codaedorme/pi/PiApplication.java
@@ -1,5 +1,6 @@
 package br.com.codaedorme.pi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import br.com.codaedorme.pi.domain.usuario.UsuarioMenu;
 @SpringBootApplication
 public class PiApplication implements CommandLineRunner {
 
+	@Autowired
 	private UsuarioMenu menu;
 
 	public static void main(String[] args) {
