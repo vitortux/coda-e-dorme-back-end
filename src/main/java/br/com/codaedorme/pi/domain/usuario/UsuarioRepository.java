c89697a3a53659a6ff0d77a358b8f99b10027c66
William Araujo <will.araujo777@gmail.com>
2025-02-23 20:20:26 -0300

    Feat: Adicionando a função de login do usuario

--- src/main/java/br/com/codaedorme/pi/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/codaedorme/pi/domain/usuario/UsuarioRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	@Override
 	Page<Usuario> findAll(Pageable pageable);
+
+	Optional<Usuario> findByEmail(String email);
 }
--- src/main/java/br/com/codaedorme/pi/domain/usuario/UsuarioService.java
+++ src/main/java/br/com/codaedorme/pi/domain/usuario/UsuarioService.java
@@ -7,6 +7,8 @@ import br.com.codaedorme.pi.domain.usuario.enums.Status;
 import br.com.codaedorme.pi.infra.criptografia.CriptografaSenha;
 import br.com.codaedorme.pi.infra.validation.ValidaSenha;
 
+import java.util.Optional;
+
 @Service
 public class UsuarioService {
 
@@ -50,4 +52,15 @@ public class UsuarioService {
 		usuario.setStatus(usuario.getStatus().equals(Status.ATIVO) ? Status.INATIVO : Status.ATIVO);
 		repository.save(usuario);
 	}
+
+	public Optional<Usuario> login(String email, String password) {
+		Optional<Usuario> existingUser = repository.findByEmail(email);
+
+		if (existingUser.isPresent() && validador.validaHash(password, existingUser.get().getId())) {
+			return existingUser;
+		}
+
+		return Optional.empty();
+	}
+
 }
